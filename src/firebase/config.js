5364191e765ea11d14d14791ffca661642fbda6e
Pablo Gallina <pablogallinag@gmail.com>
2023-05-21 17:11:29 -0600

    add firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn5Fohi-xvq24FP-j_Ecd6eGze1PHebuM",
+  authDomain: "eventia-9234e.firebaseapp.com",
+  projectId: "eventia-9234e",
+  storageBucket: "eventia-9234e.appspot.com",
+  messagingSenderId: "57771799166",
+  appId: "1:57771799166:web:f809715ed32cfbe3fb5e2f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
