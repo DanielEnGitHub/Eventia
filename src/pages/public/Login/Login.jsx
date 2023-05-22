8a406097973f303bcc7629e61b6a52a8b29100a5
Daniel <danieljgallina@gmail.com>
2023-05-21 19:10:42 -0600

    add set to responsive mode

--- src/pages/public/Login/Login.jsx
+++ src/pages/public/Login/Login.jsx
@@ -1,21 +1,14 @@
-import { useForm } from "react-hook-form";
-
-import { useDispatch, useSelector } from "react-redux";
-
-import { loginToApp, selectLoading } from "../../../redux/features/userSlice";
-
-import { Flex, Heading, Image, Stack } from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { loginToApp, selectLoading } from "../../../redux/features/userSlice";
+import { Flex, Image, Stack, Show, Hide, Box } from "@chakra-ui/react";
 import { bg } from "../../../utils/Utils";
-
 import InputFormValidation from "../../../components/Inputs/InputFormValidation/InputFormValidation.jsx";
-// import { PasswordIcon, PerfilIcon } from "../../../Utils/icons.js";
 import Button from "../../../components/Buttons/Button/Button.jsx";
 import Title from "../../../components/Texts/Title/Title.jsx";
 import TextContent from "../../../components/Texts/TextContent/TextContent.jsx";
 import { EmailIcon, LockIcon } from "@chakra-ui/icons";
-// const regEmail =
-//   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 const Login = () => {
   const loading = useSelector(selectLoading);
@@ -32,9 +25,11 @@ const Login = () => {
 
   return (
     <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-      <Flex flex={1}>
-        <Image alt={"Login Image"} objectFit={"cover"} src={bg} />
-      </Flex>
+      <Hide below="md">
+        <Flex flex={1}>
+          <Image borderRadius={"0px 14px 14px 0px"} alt={"Login Image"} objectFit={"cover"} src={bg} />
+        </Flex>
+      </Hide>
 
       <Flex p={8} flex={1} align={"center"} justify={"center"}>
         <Stack spacing={4} w={"full"} maxW={"md"}>
