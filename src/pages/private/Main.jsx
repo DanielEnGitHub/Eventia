9b08151668206e1c4673464b3fd3721f140bbdd0
Daniel <danieljgallina@gmail.com>
2023-05-30 20:05:27 -0600

    add button logout responsive

--- src/pages/private/Main.jsx
+++ src/pages/private/Main.jsx
@@ -1,6 +1,16 @@
-import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  useBreakpointValue,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import SideBar from "../../components/Layout/SideBar";
+import { Ilogout } from "../../utils/Utils";
 
 const smVariant = { navigation: "drawer", navigationButton: true };
 const mdVariant = { navigation: "sidebar", navigationButton: false };
@@ -24,6 +34,24 @@ const Main = ({ children }) => {
     >
       {/* ----Grid item for sidebar---- */}
       <SideBar area={"side"} variant={variants?.navigation} />
+
+      <Box
+        w={16}
+        h={16}
+        align={"center"}
+        justify={"center"}
+        color={"white"}
+        rounded={"full"}
+        bg={"brand.secondary"}
+        position={"absolute"}
+        bottom="5"
+        right="5"
+        className="hide"
+      >
+        <Center h="full">
+          <Image src={Ilogout} w="25px" />
+        </Center>
+      </Box>
 
       <GridItem area={"main"} maxHeight="100vh" overflow="auto" px="2" mb="5">
         {children}
