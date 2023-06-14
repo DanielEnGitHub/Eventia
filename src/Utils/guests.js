11ef3e4bbc08e785f94443529c31931b15875579
Daniel <danieljgallina@gmail.com>
2023-06-14 00:48:12 -0600

    delete button bulk

--- src/Utils/guests.js
+++ src/Utils/guests.js
@@ -1,5 +1,203 @@
 export const guests_bulk = [
   {
+    general_name: "Esposos Castellanos",
+    guests: [
+      {
+        id: 1,
+        full_name: "Yeimee Castellanos",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Esposo",
+        attend: false,
+      },
+    ],
+    table_number: "12",
+    invitation_code: "EYR1XR4",
+    registered: false,
+  },
+
+  {
+    general_name: "Esposos Valencia Gómez",
+    guests: [
+      {
+        id: 1,
+        full_name: "Byron Valencia",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Eugenia Gómez de Valencia",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Esposos Gallina Gil",
+    guests: [
+      {
+        id: 1,
+        full_name: "Raúl Gallina",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Felipa Gil de Gallina",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Julajuj Valencia",
+    guests: [
+      {
+        id: 1,
+        full_name: "Pablo Julajuj",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Loida Valencia de Julajuj",
+        attend: false,
+      },
+      {
+        id: 3,
+        full_name: "Santiago Julajuj Valencia",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Ordoñez Valencia",
+    guests: [
+      {
+        id: 1,
+        full_name: "Luis Ordoñez",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Debora Valencia de Ordoñez",
+        attend: false,
+      },
+      {
+        id: 3,
+        full_name: "David Ordoñez Valencia",
+        attend: false,
+      },
+      {
+        id: 4,
+        full_name: "Rebeca Ordoñez Valencia",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Valencia Uluán",
+    guests: [
+      {
+        id: 1,
+        full_name: "Jonathan Valencia",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Rosario Uluán de Valencia",
+        attend: false,
+      },
+      {
+        id: 3,
+        full_name: "Daniel Valencia Uluán",
+        attend: false,
+      },
+      {
+        id: 4,
+        full_name: "Valentina Valencia Uluán",
+        attend: false,
+      },
+      {
+        id: 5,
+        full_name: "Raquel Valencia Uluán",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Valencia López",
+    guests: [
+      {
+        id: 1,
+        full_name: "Pablo Valencia",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Daphne López de Valencia",
+        attend: false,
+      },
+      {
+        id: 3,
+        full_name: "Roger Valencia López",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Sr. Rodolfo Gómez",
+    guests: [
+      {
+        id: 1,
+        full_name: "Demetria Vasquez",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Rodolfo Gómez",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
+    general_name: "Esposos Gómez Gónzalez",
+    guests: [
+      {
+        id: 1,
+        full_name: "Luis Gómez",
+        attend: false,
+      },
+      {
+        id: 2,
+        full_name: "Rosa Gónzalez de Gómez",
+        attend: false,
+      },
+    ],
+    table_number: "",
+    invitation_code: "",
+    registered: false,
+  },
+  {
     general_name: "Sr. Rafael Gómez",
     guests: [
       {
--- src/pages/private/Admin/Admin.jsx
+++ src/pages/private/Admin/Admin.jsx
@@ -4,13 +4,10 @@ import HeaderAdmin from "../../../components/HeaderInfo/HeaderAdmin";
 import Card from "../../../components/Card";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  bulkCreateGuests,
   getGuestAll,
   guestAll,
   selectLoadingdGuests,
 } from "../../../redux/features/guestsSlice";
-import Button from "../../../components/Buttons/Button";
-import { guests_bulk } from "../../../Utils/guests";
 
 const Admin = () => {
   const selectGuest = useSelector(guestAll);
@@ -21,16 +18,10 @@ const Admin = () => {
     dispatch(getGuestAll(""));
   }, []);
 
-  const handleBulkCreateGuests = () => {
-    dispatch(bulkCreateGuests(guests_bulk));
-  };
-
   return (
     <>
       <Box p="0 10px 0 10px" m="0">
         <HeaderAdmin title="INVITADOS" textButton="Agregar Invitado" />
-
-        <Button text="Bulk Agregar Invitado" primary onClick={handleBulkCreateGuests} />
 
         <Container maxW={"full"} p="0" mt={12}>
           {loading_guest ? (
